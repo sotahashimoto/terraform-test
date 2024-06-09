b04a517042f76099296f1e19c1d5f5982f260ddb
s_hashimoto <sotahashimoto316@gmail.com>
2024-06-10 02:35:11 +0900

    a

--- terraform/moduels/ec2/variable.tf
+++ terraform/moduels/ec2/variable.tf
@@ -1,13 +1,3 @@
 variable "vpc_id" { type = string }
 
-variable "sg_rule" {
-  type = map(object({
-    type                     = string
-    from_port                = number
-    to_port                  = number
-    protocol                 = string
-    source_security_group_id = optional(string, null)
-    cidr_blocks              = optional(string, null)
-    description              = string
-  }))
-}
+variable "sg_rule" { type = map(any)}
