16a84e4141b42c10bb0bcfb323c47b73d8c31b7f
s_hashimoto <sotahashimoto316@gmail.com>
2024-06-10 02:20:31 +0900

    s

--- terraform/moduels/ec2/variable.tf
+++ terraform/moduels/ec2/variable.tf
@@ -6,8 +6,8 @@ variable "sg_rule" {
     from_port                = number
     to_port                  = number
     protocol                 = string
-    source_security_group_id = list(string)
-    cidr_blocks              = list(string)
+    source_security_group_id = optional(string)
+    cidr_blocks              = optional(string)
     description              = string
   }))
 }
