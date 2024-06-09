2c89828adb93c3f3e15641b263d31659d930d827
s_hashimoto <sotahashimoto316@gmail.com>
2024-06-10 02:40:38 +0900

    a

--- terraform/moduels/ec2/main.tf
+++ terraform/moduels/ec2/main.tf
@@ -16,13 +16,7 @@ resource "aws_security_group_rule" "this" {
   from_port                = each.value[1]
   to_port                  = each.value[2]
   protocol                 = each.value[3]
+  source_security_group_id = each.value[4]
+  cidr_blocks              = each.value[5]
   description              = each.value[6]
-
-  dynamic "source_security_group_id" {
-    for_each = each.value[4] != null ? [each.value[4]] : []
-  }
-
-  dynamic "cidr_blocks" {
-    for_each = length(each.value[5]) > 0 ? [each.value[5]] : []
-  }
 }
