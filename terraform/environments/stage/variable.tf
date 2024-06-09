6a657edd0ccac99c291ffff5f947ff27ffdcbd24
s_hashimoto <sotahashimoto316@gmail.com>
2024-06-10 02:22:39 +0900

    d

--- terraform/environments/stage/variable.tf
+++ terraform/environments/stage/variable.tf
@@ -6,7 +6,7 @@ locals {
     ## [ type, from_port, to_port, protocol, sg-id, cidr_blocks, description ]
     "ingress_rule_1" = ["ingress", 80, 80, "tcp", null, ["0.0.0.0/0"], "HTTP"],
     "ingress_rule_2" = ["ingress", 443, 443, "tcp", null, ["0.0.0.0/0"], "HTTPS"],
-    "ingress_rule_3" = ["ingress", 22, 22, "tcp", "sg-0b0966a99d7eb773c", null, "SSH from Bastion"],
+    "ingress_rule_3" = ["ingress", 22, 22, "tcp", "sg-0b0966a99d7eb773c", [], "SSH from Bastion"],
     "egress_rule_1" = ["egress", 0, 0, "-1", null, ["0.0.0.0/0"], "test"]
   }
 }
